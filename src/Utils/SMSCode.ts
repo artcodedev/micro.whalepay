774ad03a2843126edf9303604873547cff079cf9
art[code] <you@example.com>
2024-12-25 13:20:19 +1000

    12

--- src/Utils/SMSCode.ts
+++ src/Utils/SMSCode.ts
@@ -3,8 +3,6 @@ import { Fetch } from "./Fetch";
 import { Logger } from "./Logger";
 import { Token } from "./Token";
 
-
-
 interface requestSMSRes {
     status: number
     data: FetchSMS[]
@@ -19,35 +17,76 @@ interface FetchSMS {
 
 interface requestSMSAnswer {
     status: boolean
-    data?: FetchSMS[]
+    data?: string
 }
 
 export class SMSCode {
 
-    private static async parseAllSMS() { }
-
-    private static async requestSMS(): Promise<requestSMSAnswer> {
+    public static async deleteSMS(): Promise<requestSMSAnswer> {
 
         try {
             const token: string = await Token.sign({ uid: 'uid' }, SecretKey.secret_key_micro, 1000);
 
-            const req: requestSMSRes = await Fetch.request('http://localhost:3005/getallmessages', {token: token});
+            const req: requestSMSRes = await Fetch.request('http://localhost:3005/deleteallmessage', { token: token });
 
-            if (req.status) {}
-
-            return {status: false}
+            return { status: req.status ? true : false }
 
         }
 
         catch (e) {
             Logger.write(process.env.ERROR_LOGS, e);
-            return {status: false}
+            return { status: false }
         }
     }
 
-    public static async getSMS(phone: string) {
+    public static async getSMS(): Promise<requestSMSAnswer> {
 
+        try {
 
+            const token: string = await Token.sign({ uid: 'uid' }, SecretKey.secret_key_micro, 1000);
 
+            const req: requestSMSRes = await Fetch.request('http://localhost:3005/getallmessages', { token: token });
+
+            if (req.status) {
+
+                if (req.data.length) {
+                    const num: string[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+                    const code_sms: string[] = []
+
+                    for (let i of req.data.reverse()) {
+
+                        const message: string = i.message.toLowerCase();
+
+                        if (message.includes("код")) {
+                            const position = message.indexOf('код');
+
+                            for (let p = position; p < message.length; p++) {
+
+                                if (num.includes('.')) {
+                                    break
+                                }
+
+                                if (num.includes(message[p])) {
+                                    code_sms.push(message[p])
+                                }
+                            }
+
+                        }
+                    }
+
+                    return {status: true, data: code_sms.join('')}
+                }
+            }
+
+            return { status: false }
+
+        }
+
+        catch (e) {
+            Logger.write(process.env.ERROR_LOGS, e);
+            return { status: false }
+        }
     }
+
 } 
